5caa173e462cd0af9c63ad0c2e5f03e0824b435f
Merist5 <37301295+Merist5@users.noreply.github.com>
2018-06-05 22:19:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -62,5 +62,6 @@ var Counter = React.createClass({
 var element =
   React.createElement('div', {},
     React.createElement(Counter, {}),
+    React.createElement(Counter, {}),
   );
 ReactDOM.render(element, document.getElementById('app'));
